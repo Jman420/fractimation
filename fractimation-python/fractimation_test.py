c673707eb2cbacf1fcafe15d030f3785ae343e1c
Justin Giannone <jgiannone@mdsol.com>
2018-02-11 18:46:37 -0500

    Re-enable all tests

--- fractimation-python/fractimation_test.py
+++ fractimation-python/fractimation_test.py
@@ -15,7 +15,7 @@ colorMap = "viridis"                                   # Any valid color map nam
                                                        # ^^ reference : https://matplotlib.org/examples/color/colormaps_reference.html
 
 # Mandelbrot Set
-#multibrotViewer = plotplayer.plotplayer("Mandelbrot Set")
+multibrotViewer = plotplayer.plotplayer("Mandelbrot Set")
 realNumberMin, realNumberMax = -2.0, 0.5               # Min & Max values for X values in fractal equation
 imaginaryNumberMin, imaginaryNumberMax = -1.25, 1.25   # Min & Max values for Y values in fractal equation
 constantRealNumber, constantImaginaryNumber = 0.0, 0.0 # Needs some experimentation in Multibrot Set (non-standard)
@@ -23,11 +23,11 @@ power = 2                                              # Power to raise Z value 
 escapeValue = 2.0                                      # Limit at which Z values will reach infinity
 multibrotRenderer = multibrotRenderer(width, height, realNumberMin, realNumberMax, imaginaryNumberMin, imaginaryNumberMax, 
                                       constantRealNumber, constantImaginaryNumber, power, escapeValue, colorMap)
-#multibrotViewer.initializeAnimation(maxIterations, multibrotRenderer.render)
-#multibrotZoomHandler = zoomHandler(multibrotRenderer, multibrotViewer)
+multibrotViewer.initializeAnimation(maxIterations, multibrotRenderer.render)
+multibrotZoomHandler = zoomHandler(multibrotRenderer, multibrotViewer)
 
 # Julia Set
-#multijuliaViewer = plotplayer.plotplayer("Julia Set")
+multijuliaViewer = plotplayer.plotplayer("Julia Set")
 realNumberMin, realNumberMax = -1.5, 1.5
 imaginaryNumberMin, imaginaryNumberMax = -1.5, 1.5
 constantRealNumber, constantImaginaryNumber = 0.0, 0.8 # Constant C value for Julia Set
@@ -35,8 +35,8 @@ power = 2
 escapeValue = 10.0
 multijuliaRenderer = multijuliaRenderer(width, height, realNumberMin, realNumberMax, imaginaryNumberMin, imaginaryNumberMax,
                                         constantRealNumber, constantImaginaryNumber, power, escapeValue, colorMap)
-#multijuliaViewer.initializeAnimation(maxIterations, multijuliaRenderer.render)
-#juliaZoomHandler = zoomHandler(multijuliaRenderer, multijuliaViewer)
+multijuliaViewer.initializeAnimation(maxIterations, multijuliaRenderer.render)
+juliaZoomHandler = zoomHandler(multijuliaRenderer, multijuliaViewer)
 
 # Sierpinski Triangle
 sierpinskiIterations = 8
@@ -45,11 +45,11 @@ sierpinskiTriangleRenderer = sierpinskiTriangleRenderer(width, height, sierpinsk
 sierpinskiTriangleViewer.initializeAnimation(sierpinskiIterations, sierpinskiTriangleRenderer.render, frameRate=sierpinskiIterations // 2)
 
 # Render Viewers and Play Animations
-#multibrotViewer.show(False)
-#multibrotViewer.play()
+multibrotViewer.show(False)
+multijuliaViewer.show(False)
 
-#multijuliaViewer.play()
-#multijuliaViewer.show()
-
+multibrotViewer.play()
+multijuliaViewer.play()
 sierpinskiTriangleViewer.play()
+
 sierpinskiTriangleViewer.show()
